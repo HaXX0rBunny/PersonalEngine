29de1c169f001f8000e0585df0dd16da6ea4e831
HaXX0rBunny <131460065+HaXX0rBunny@users.noreply.github.com>
2024-10-05 19:53:53 +0900

    Shader constructor scripting

--- PersonalEngine/Shader.cpp
+++ PersonalEngine/Shader.cpp
@@ -12,11 +12,25 @@ Shader::Shader(const GLchar* vertexPath, const GLchar* fragmentPath)
 	fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 	try
 	{
+		vShaderFile.open(vertexPath);
+		fShaderFile.open(fragmentPath);
 
-		//https://heinleinsgame.tistory.com/8
+		std::stringstream vShaderStream, fShaderStream;
+		// Read file buffer in Stream
+		vShaderStream << vShaderFile.rdbuf();
+		fShaderStream << fShaderFile.rdbuf();
+		//Closd file handler
+		vShaderFile.close();
+		fShaderFile.close();
+		//Convert string to Stream
+		vertexCode = vShaderStream.str();
+		fragmentCode = fShaderStream.str();
+		
 	}
 	catch (std::ifstream::failure e)
 	{
-	
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
 	}
+	const char* vShaderCode = vertexCode.c_str();
+	const char* fShaderCode = fragmentCode.c_str();
 }
